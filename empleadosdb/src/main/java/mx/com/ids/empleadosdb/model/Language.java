278ce0138aefc4277e2ebb15e28d906fcefb683e
DemianX04 <demianx04@gmail.com>
2022-07-04 19:15:28 -0500

    application properties actualizado

--- empleadosdb/src/main/java/mx/com/ids/empleadosdb/model/Language.java
+++ empleadosdb/src/main/java/mx/com/ids/empleadosdb/model/Language.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +17,7 @@ public class Language  implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	@ManyToOne
+	@ManyToMany
 	@JoinColumn(referencedColumnName = "id", name = "id")
 	private Employee id;
 	
